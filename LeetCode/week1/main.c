887d618ff3cd7b5c238b82a7540eea179bf25745
dawnherald <91484731+dawnherald@users.noreply.github.com>
2021-12-05 19:35:37 +0800

    Update main.c

--- LeetCode/week1/main.c
+++ LeetCode/week1/main.c
@@ -1,1 +1,18 @@
-dss
+ 
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int i, j;
+    for (i = 0; i < numsSize; i++) {
+        for (j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                int* ret = malloc(sizeof(int) * 2);
+                ret[0] = i, ret[1] = j;
+                *returnSize = 2;
+                return ret;
+            }
+        }
+
+    }
+
+}
